69f42609136ef5fb638da00cbcff61071b3e5004
Takuma4869 <e18-0094bukkyo-u-ac.jp>
2021-07-27 18:19:23 +0900

    updated config file

--- classes/Config.php
+++ classes/Config.php
@@ -1,10 +1,10 @@
 <?php
 class Config
 {
-    private $servername = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $db_name = 'concert_information_db';
+    private $servername = 'us-cdbr-east-04.cleardb.com';
+    private $username = 'be3c38f7a9103f';
+    private $password = '03d84b5f';
+    private $db_name = 'heroku_e6f7a2138fd4542';
     public $conn;
 
     public function __construct()
