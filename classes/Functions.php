7c0f49d803932b345bbffb175076fed6246137ca
Takuma4869 <e18-0094bukkyo-u-ac.jp>
2021-07-28 16:24:30 +0900

    updated

--- classes/Functions.php
+++ classes/Functions.php
@@ -380,7 +380,7 @@ class Functions extends Config
 
      public function follow_or_unfollow($user_id,$post_id)
      {
-        $sql = "SELECT * FROM post WHERE user_id = '$user_id' AND post_id = '$post_id'";
+        $sql = "SELECT * FROM favorite WHERE user_id = '$user_id' AND post_id = '$post_id'";
         $result = $this->conn->query($sql);
 
         if ($result->num_rows == 1) {
